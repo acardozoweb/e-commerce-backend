c6712ec46382ebe3edfa5126d9fc3fafef2c1a57
anthonycc <anthonyccardozo@gmail.com>
2021-09-14 16:33:10 -0400

    testing product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then(data => {
+    console.log('find all products')
+    res.json(data)});
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag
+      }
+    ]
+  })
+  .then(data => {
+    console.log('find product by id')
+    res.json(data)
+  });
 });
 
 // create new product
@@ -91,6 +121,12 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params/id
+    }
+  })
+  .then(data => res.json(data))
 });
 
 module.exports = router;
